aa68c0aeb21955b81f26f1f11af253b0e6b8a29b
Sayali Chavan <92865767+sayali7chavan@users.noreply.github.com>
2023-08-13 17:11:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const countdownDate = new Date("2023-12-31T23:59:59").getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
